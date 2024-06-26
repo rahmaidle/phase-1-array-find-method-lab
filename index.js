ef9ca28f66566cec7b8ceca4b51e5bf8f260bd11
rahmaidle <rahmaidle254@gmail.com>
2024-06-26 12:39:57 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+// index.js
+// index.js
+
+function superbowlWin(record) {
+    const win = record.find(function(game) {
+      return game.result === 'W';
+    });
+  
+    return win? win.year : undefined;
+  }
+  
+  module.exports = superbowlWin;
